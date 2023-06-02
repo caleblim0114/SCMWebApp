e33fbcfb1128b398db26c45f92e31a4f6015c5df
yung7 <yung7@180CM>
2023-06-03 02:37:15 +0800

    UI binding push - in progress

--- SCMWebApp/Pages/Index.cshtml.cs
+++ SCMWebApp/Pages/Index.cshtml.cs
@@ -19,15 +19,61 @@ namespace SCMWebApp.Pages
         [BindProperty]
         public List<Banner> Banners { get; set; } = new List<Banner>();
 
+        [BindProperty]
+        public Banner About_Banner { get; set; }
+
+        [BindProperty]
+        public Staff? BMD_Staff { get; set; } = new();
+
+        [BindProperty]
+        public Staff? BCS_Staff { get; set; } = new();
+
+        [BindProperty]
+        public Staff? BID_Staff { get; set; } = new();
+
+        [BindProperty]
+        public Staff? BDM_Staff { get; set; } = new();
+
         public async void OnGet()
         {
             try
             {
-                var banner =  _databaseContext.Banner
-                    .Where(x=>x.BannerTypeId == 1)
+                var banner = _databaseContext.Banner
+                    .Where(x => x.BannerTypeId == 1)
                     .ToList();
 
                 Banners = banner;
+
+                var bmd_staff = _databaseContext.Staff
+                    .Where(x => x.ProgrammeId == 1)
+                    .FirstOrDefault();
+
+                BMD_Staff = bmd_staff;
+
+                var bcs_staff = _databaseContext.Staff
+                    .Where(x => x.ProgrammeId == 2)
+                    .FirstOrDefault();
+
+                BCS_Staff = bcs_staff;
+
+                var bid_staff = _databaseContext.Staff
+                    .Where(x => x.ProgrammeId == 3)
+                    .FirstOrDefault();
+
+                BID_Staff = bid_staff;
+
+                var bdm_staff = _databaseContext.Staff
+                    .Where(x => x.ProgrammeId == 4)
+                    .FirstOrDefault();
+
+                BDM_Staff = bdm_staff;
+
+                var about_banner = _databaseContext.Banner
+                    .Where(x => x.BannerTypeId == 4)
+                    .FirstOrDefault();
+
+                About_Banner = about_banner;
+
             }
             catch (Exception ex)
             {
--- SCMWebApp/Pages/PostgraduatePage.cshtml.cs
+++ SCMWebApp/Pages/PostgraduatePage.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SCMWebApp.Shared.Models;
 
 namespace SCMWebApp.Pages
 {
     public class PostgraduatePageModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<PostgraduatePageModel> _logger;
+        private SCMWebAppDatabaseContext _databaseContext;
+
+        public PostgraduatePageModel(ILogger<PostgraduatePageModel> logger, SCMWebAppDatabaseContext databaseContext)
         {
+            _logger = logger;
+            _databaseContext = databaseContext;
+        }
+
+        [BindProperty]
+        public Staff SPSStaff { get; set; } = new();
+
+        public async void OnGet()
+        {
+            try
+            {
+                var spsStaff = _databaseContext.Staff
+                    .Where(x => x.ProgrammeId == 6)
+                    .Include(x => x.Position)
+                    .FirstOrDefault();
+                if (spsStaff != null)
+                {
+                    SPSStaff = spsStaff;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
