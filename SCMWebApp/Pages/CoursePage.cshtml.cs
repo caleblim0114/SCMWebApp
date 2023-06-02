87e6deb19f27ed2909d63155ab544005cbedea70
yung7 <yung7@180CM>
2023-06-02 09:37:39 +0800

    WebApp UI Push- in progress

---
+++ SCMWebApp/Pages/CoursePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SCMWebApp.Pages
+{
+    public class CoursePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
